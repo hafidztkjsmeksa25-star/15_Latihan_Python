f1fb1f00741c24807c2c5ca08a22f5d1f2357e59
15_Hafidz Muqorrobin Al-Musthofa <hafidz.tkjsmeksa25@gmail.com>
2026-08-31 09:38:24 +0700

    fungsion
    Refactor code to use a function for checking even/odd numbers and add error handling for invalid input.

--- Latihan_Python.py
+++ Latihan_Python.py
@@ -1,6 +1,16 @@
-while True:
-    x = int(input("Masukkan Nilai X: "))
-    if x % 2 == 0:
-        print(f"Bilangan {x} termasuk Bilangan genap")
-    else:
-        print(f"Bilangan {x} termasuk Bilangan ganjil")
+def bilangan_genap_ganjil():
+    while True:
+        try:
+            x = int(input("Masukkan Nilai X: "))
+        except ValueError:
+            print("Input tidak valid, masukkan angka!")
+            continue
+
+        if x % 2 == 0:
+            print(f"Bilangan {x} termasuk Bilangan genap")
+        else:
+            print(f"Bilangan {x} termasuk Bilangan ganjil")
+
+
+if __name__ == "__main__":
+    bilangan_genap_ganjil()
