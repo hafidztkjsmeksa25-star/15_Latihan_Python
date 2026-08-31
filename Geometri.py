db92db0752bcb569b6d41da1983b89f8252dc27c
15_Hafidz Muqorrobin Al-Musthofa <hafidz.tkjsmeksa25@gmail.com>
2026-08-31 19:56:37 +0700

    Add files via upload

---
+++ Geometri.py
@@ -0,0 +1,11 @@
+def luas_persegi(sisi):
+    return sisi * sisi
+
+def keliling_persegi(sisi):
+    return 4 * sisi
+
+def luas_lingkaran(r):
+    return 3.14 * r * r
+
+def luas_segitiga(alas, tinggi):
+    return 0.5 * alas * tinggi
